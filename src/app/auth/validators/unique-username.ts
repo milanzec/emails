ccd13f6636da0ebf7da1eb744339ec24486cd17b
unknown <milanzec53@gmail.com>
2023-09-26 14:52:42 +0200

    http validate

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -12,9 +12,9 @@ export class UniqueUsername implements AsyncValidator {
     validate = (control: AbstractControl): any => {
         const { value } = control
 
-        console.log(this.http)
-
-        return null
+        return this.http.post<any>('https://api.angular-email.com/auth/username', {
+            username: value
+        })
 
     }
 }
