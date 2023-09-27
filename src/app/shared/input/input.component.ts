292a10efbd15ce5d4ccec76e94bf9e03708b4019
unknown <milanzec53@gmail.com>
2023-09-27 15:04:59 +0200

    input customization

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -11,5 +11,12 @@ export class InputComponent {
 
   @Input() label: string = ''
   @Input() control: FormControl
+  @Input() inputType: string = ''
+
+  showErrors() {
+    const { dirty, touched, errors } = this.control
+
+    return dirty && touched && errors
+  }
 
 }
